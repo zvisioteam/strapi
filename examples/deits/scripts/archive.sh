39e0a475f426fa87101e310f91e9c345b03f8c4a
Convly <jean-sebastien.herbaux@epitech.eu>
2022-09-30 17:48:57 +0200

    Update archive script to create out dir if it doesn't exist

--- examples/deits/scripts/archive.sh
+++ examples/deits/scripts/archive.sh
@@ -5,8 +5,13 @@ BLUE='\033[0;34m'
 LIGHTGREEN='\033[1;32m'
 DARKGRAY='\033[1;30m'
 
-OUT_FILE=out/backup.tar
+OUT_DIR=out
+OUT_FILE=backup.tar
+OUT_PATH=$OUT_DIR/$OUT_FILE
 
-tar -cf out/backup.tar -C resource/backup-data-example $(ls -A resource/backup-data-example)
-gzip $OUT_FILE -f
-echo "${LIGHTGREEN}✔️ Success ${DARKGRAY}(See output at ${OUT_FILE})${NOCOLOR}"
+ARCHIVE_PATH=resource/backup-data-example
+
+mkdir -p $OUT_DIR
+tar -cf $OUT_PATH -C $ARCHIVE_PATH $(ls -A $ARCHIVE_PATH)
+gzip $OUT_PATH -f
+echo "${LIGHTGREEN}✔️ Success ${DARKGRAY}(See output at ${OUT_PATH}.gz)${NOCOLOR}"
